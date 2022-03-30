f8c21d9677ad544eb9f2c575b38e1996d6e097bc
Arturito <arturaskisielius91@gmail.com>
2022-03-31 00:05:05 +0300

    #9 papildomas is KP

--- nd/01-nd.js
+++ nd/01-nd.js
@@ -262,5 +262,13 @@ if (kauliukas % 2 === 0) {
 }
 console.log('-------------------------');
 
+// 9. Pirmosios olimpinės žaidynės įvyko 1896 metais ir toliau organizuojamos kas ketveri metai. Jei žaidynės neįvyksta, tie metai vis tiek laikomi olimpiniais, o žaidynėms skiriamas eilės numeris. Parašykite programą, kuri surastų m-ųjų metų olimpinių žaidynių numerį n. Jei metai neolimpiniai, turi būti spausdinama „Metai neolimpiniai“. Pasitikrinkite. Kai m = 1904, turi būti spausdinama: n = 3. Kai m = 2005, turi būti spausdinama: Metai neolimpiniai.
 
+let m1 = 1904;
+let nNum = (m1 - 1896) / 4;
 
+console.log('n =', ++nNum);
+
+if (m1 % 4 !== 0) {
+    console.log('Metai neolimpiniai');
+} 

fe6977d2e4aa4504558f827d7914f0d12828e593
Arturito <arturaskisielius91@gmail.com>
2022-04-03 19:57:12 +0300

    for #1

---
+++ nd/0331-nd.js
@@ -0,0 +1,36 @@
+// #1
+
+//a)
+for (let i = 0; i === 0; i++) {
+    console.log(i);
+}
+//b)
+let sumaB = 0;
+for (let i = 0; i <= 4; i++) {
+    sumaB = sumaB + i;
+}
+console.log(sumaB);
+//c)
+let sumaC = 0;
+for (i = 0; i <= 100; i++) {
+    sumaC = sumaC + i;
+}
+console.log(sumaC);
+//d)
+let sumaD = 0;
+for (i = 574; i <= 815; i++) {
+    sumaD = sumaD + i;
+}
+console.log(sumaD);
+//e)
+let sumaE = 0;
+for (i = -50; i <= 50; i++) {
+    sumaE = sumaE + i;
+}
+console.log(sumaE);
+//f)
+let sumaF = 0;
+for (i = -70; i <= 30; i++) {
+    sumaF = sumaF + i;
+}
+console.log(sumaF);

c65ae44128d16beddb820d38cfc673db982ce47f
Arturito <arturaskisielius91@gmail.com>
2022-04-08 17:50:44 +0300

    Array #4, 5

--- nd/0407-nd-array.js
+++ nd/0407-nd-array.js
@@ -8,6 +8,9 @@ while (arr.length < 20) {
 }
 console.log(arr);
 
+arr = Array.from({length: 20}, () => Math.floor(Math.random() * 20) + 1);
+console.log(arr);
+
 // // 2 budas
 // arr = new Array(20).fill();
 // arrRandom = arr.map(() => Math.floor(Math.random() * 20) + 1);
@@ -16,22 +19,23 @@ console.log(arr);
 
 
 // 2. Sukurti atsitiktinio ilgio masyvą, nuo 40 iki 60, kurio reikšmės būtų skaičiai nuo 1 iki masyvo ilgio.
-// console.log('---------------[2]------------------')
-// start = 40;
-// end = 60;
+console.log('---------------[2]------------------')
 
-// arr = [];
-// while () {
-//     arrRandom = Math.floor(Math.random(60 - 40 + 1));
-//     arr.push(arrRandom);
-// }
-// console.log(arr);
+minLength = 40;
+maxLength = 60;
 
+arr.length = Math.floor(Math.random() * (maxLength - minLength + 1) + minLength);
+
+minNum = 1;
+maxNum = arr.length;
+
+arr = Array.from({length: arr.length}, () => Math.floor(Math.random() * (maxNum - minNum + 1) + minNum));
+
+console.log(arr);
 
 // 3. Sukurti atsitiktinio ilgio masyva, kurio ilgis nuo 10 iki 15, o reikšmės – atsitiktiniai skaičiai nuo 5 iki 47. Surasti didžiausia skaičių masyve, nenaudojant sort ir Math.max funkcijų.
 console.log('---------------[3]------------------')
 
-arr = [];
 minLength = 10;
 maxLength = 15;
 maxNum = 5;
@@ -52,39 +56,38 @@ console.log(`Didžiausias skaičius masyve: ${maxReiksme}.`);
 // 4. Sukurti masyvą, kurio ilgis 100, o reikšmės – raidės D, E, F, G, H. 20. 
 console.log('---------------[4]------------------')
 
-arr = new Array(20).fill('D, E, F, G, H');
-console.log(arr);
+const myArray = [];
+letters = 'DEFGH';
+lettersArray = letters.split('');
 
-arr = [];
-raides = 'D, E, F, G, H'
-while (arr.length < 100) {
-    
-    raidesSplit = raides.split('')
-    arrRandom = Math.floor(Math.random() * 10) + 1;
-    arr.push(arrRandom);
-    }
-console.log(raidesSplit);
+for (let i = 0; i < 100 / lettersArray.length ; i++) {
+    myArray.push(...lettersArray);
+}
 
-
-
-
+console.log(myArray);
 
 // 5. Sukurti masyvą, kurio ilgis 100, o reikšmės – raidės M,N,O,P. Suskaičiuoti, kurios raidės buvo daugiausia.
-// console.log('---------------[5]------------------')
-// arr = [];
-// while (arr.length < 100) {
+console.log('---------------[5]------------------')
 
+const thisArray = [];
+letters = 'MNOP';
+lettersArray = letters.split('');
+
+for (let i = 0; i < 100 / lettersArray.length ; i++) {
+  
+  thisArray.push(...lettersArray);
+}
+
+console.log(thisArray);
 
 // 6. Sugeneruoti masyvą, kurio ilgis 20, o reikšmės masyve turi būti nuo 10 iki 50. Reikšmės masyve turi būti unikalios, tai yra – nesikartoti.
 console.log('---------------[6]------------------')
-// arr = new Array(20).fill();
 
 arr = [];
 while (arr.length < 20) {
     arrRandom = Math.floor(Math.random() * 40) + 11;
     if (arr.indexOf(arrRandom) === -1) arr.push(arrRandom);
 }
-
 console.log(arr);
 
-
+console.log('---------------------------------')
